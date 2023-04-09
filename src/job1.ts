ed7c9fc48aa4ad97260fdd71eb0ebef3c9ebdb17
Johan Borestad <johan@borestad.com>
2023-04-09 15:23:14 +0200

    🔨 BUMP: 5 files changed, 8(+), 8(-)

--- src/job1.ts
+++ src/job1.ts
@@ -12,7 +12,7 @@ await $ `echo ---------`
 
 // parallel
 await Promise.all([
-  $ `sleep 1 ; echo 1`,
-  $ `sleep 2 ; echo 2`,
-  $ `sleep 3 ; echo 3`,
+  $ `sleep 0 ; echo 1`,
+  $ `sleep 0 ; echo 2`,
+  $ `sleep 0 ; echo 3`,
 ])
