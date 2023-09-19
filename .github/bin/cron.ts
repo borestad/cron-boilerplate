46b4709078d67a6a8f13851f8cd82e6e25897e79
Johan Borestad <johan@borestad.com>
2023-09-19 22:13:48 +0200

    `️️⚡️ BUMP ⚡️`  `3 files 26(+) 1(-)`

--- .github/bin/cron.ts
+++ .github/bin/cron.ts
@@ -1,5 +1,15 @@
 #!/usr/bin/env -S deno run -A
 
-import { $ } from 'deps.ts'
+import { $, log } from 'deps.ts'
+import { bkt } from 'lib.ts'
 
 await $ `echo hello world`
+
+const param = '+%Y-%m-%d-%H%M%S'
+
+const res = await bkt({
+  to: '',
+  cmd: `date ${param}`
+})
+
+log(res.stdout)
